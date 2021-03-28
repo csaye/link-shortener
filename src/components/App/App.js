43bfbb993430fdbfd7dbf559a1c9636d8ee4ba99
Cooper Saye <coopersaye@gmail.com>
2021-03-28 16:31:15 -0600

    Set up basic app structure.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,35 @@
+import React, { useState } from 'react';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
 import './App.css';
 
+import { firebaseConfig } from '../../util/firebaseConfig.js';
+
+firebase.initializeApp(firebaseConfig);
+
 function App() {
+  const [longUrl, setLongUrl] = useState('');
+  const [shortUrl, setShortUrl] = useState('');
+
+  async function shortenLink(e) {
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
+      <div className="center-box">
+        <form onSubmit={shortenLink}>
+          <input
+            value={longUrl}
+            onChange={e => setLongUrl(e.target.value)}
+            required
+          />
+          <button type="submit">Shorten Link</button>
+        </form>
+        {shortUrl && <a href={shortUrl} rel="noreferrer"><p>{shortUrl}</p></a>}
+      </div>
     </div>
   );
 }
